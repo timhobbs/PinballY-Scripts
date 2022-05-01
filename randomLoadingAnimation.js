738590c7dcb6ed8b8a0833ae51484cf03feca632
timhobbs <jesus.tesh@gmail.com>
2022-04-30 22:46:58 -0700

    add random load animation script

---
+++ randomLoadingAnimation.js
@@ -0,0 +1,61 @@
+// START - Edit these values
+// ------------------------------------------
+
+// Whether or not to disable the wheel image when thew animation plays
+const disableWheel = true;
+
+// Whether or not to disable the messaging when thew animation plays
+const disableMessaging = true;
+
+// ------------------------------------------
+// END - Edit these values
+
+mainWindow.on("launchoverlayshow", (ev) => {
+    // Get the current game info
+    const gameInfo = ev.game;
+
+    // Don't proceed if game is not configured
+    if (!gameInfo.isConfigured) {
+        return;
+    }
+
+    // Get a random video
+    let video = getRandomVideo();
+
+    // Only bother if we have a video
+    if (!video) {
+        return;
+    }
+
+    mainWindow.launchOverlay.bg.loadVideo(video);
+    ev.preventDefault();
+});
+
+mainWindow.on("gamestarted", (ev) => {
+    mainWindow.launchOverlay.bg.clear(0xff000000);
+});
+
+mainWindow.on("launchoverlaymessage", (ev) => {
+    ev.hideWheelImage = disableWheel;
+
+    // Hide loading messages only
+    if (['terminating', 'gameover', 'after'].includes(ev.id)) {
+        return;
+    }
+    ev.message = disableMessaging ? '' : ev.message;
+});
+
+function getRandomVideo() {
+    const fso = createAutomationObject("Scripting.FileSystemObject");
+    const files = fso.GetFolder(fso.GetAbsolutePathName('../Media/Videos')).Files;
+    const videoFiles = [];
+    for (let file of files) {
+        videoFiles.push(file.Name);
+    }
+
+    return gameList.resolveMedia('Videos', videoFiles[getRandomInt(files.Count)]);
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
