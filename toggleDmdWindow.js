21f62385bc3786780f4da8e77187da9628a16c35
timhobbs <jesus.tesh@gmail.com>
2022-08-18 12:45:44 -0700

    edit and rename

--- toggleDmdWindow.js
+++
@@ -1,9 +0,0 @@
-// Disable DMD when game is running
-mainWindow.on("gamestarted", ev => {
-    setTimeout(() => {
-        dmdWindow.showWindow(false);
-    }, 10 * 1000); // wait 10 seconds - TODO: listen for actual window
-});
-
-// Turn DMD window back on when game ends
-mainWindow.on("gameover", ev => { dmdWindow.showWindow(true); });
